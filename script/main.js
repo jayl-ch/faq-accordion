97369e0d9abc4d634ac3495a63bb3299d4469718
jayl-ch <jaychicogo328@gmail.com>
2025-12-25 01:12:33 +0800

    Initial commit

---
+++ script/main.js
@@ -0,0 +1,54 @@
+const accordionBtn = document.querySelectorAll(".accordion-btn");
+const accordionWrap = document.querySelectorAll(".accordion-wrapper");
+
+function setShow(btn) {
+  const content = btn.nextElementSibling;
+  btn.querySelector("img").src = "assets/images/icon-minus.svg";
+  content.hidden = false;
+  content.style.height = "0px";
+  content.style.height = content.scrollHeight + "px";
+}
+
+function setHidden(btn) {
+  const content = btn.nextElementSibling;
+  btn.querySelector("img").src = "assets/images/icon-plus.svg";
+
+  content.style.height = "0px";
+
+  content.addEventListener(
+    "transitionend",
+    () => {
+      content.hidden = true;
+    },
+    { once: true }
+  );
+}
+
+function collapseAccordion() {
+  accordionWrap.forEach((b) => {
+    const content = b.querySelector(":scope > div");
+    b.querySelector(":scope > button > img").src =
+      "assets/images/icon-plus.svg";
+    content.hidden = true;
+    b.querySelector(":scope > button").setAttribute("aria-expanded", "false");
+  });
+}
+
+function setActiveBtn(btn) {
+  const expanded = btn.getAttribute("aria-expanded") === "true";
+  btn.setAttribute("aria-expanded", !expanded);
+
+  if (!(btn.getAttribute("aria-expanded") === "true")) {
+    setHidden(btn);
+  } else {
+    collapseAccordion();
+    btn.setAttribute("aria-expanded", "true");
+    setShow(btn);
+  }
+}
+
+accordionBtn.forEach((b) => {
+  b.addEventListener("click", (e) => {
+    setActiveBtn(e.currentTarget);
+  });
+});
